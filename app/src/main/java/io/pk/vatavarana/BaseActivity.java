318fe1edb0b86534bc91f6a2700c04ea431dbba0
Pravin Kandala <pravinkandala@hotmail.com>
2017-03-25 21:02:42 -0400

    added menu for future purpose

--- app/src/main/java/io/pk/vatavarana/BaseActivity.java
+++ app/src/main/java/io/pk/vatavarana/BaseActivity.java
@@ -1,20 +1,28 @@
 package io.pk.vatavarana;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.design.widget.TabLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 import io.pk.vatavarana.adapter.TabAdapter;
 
 public class BaseActivity extends AppCompatActivity {
+
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
@@ -27,4 +35,12 @@ public class BaseActivity extends AppCompatActivity {
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.base_toolbar_menu, menu);
+        return true;
+    }
+
 }
